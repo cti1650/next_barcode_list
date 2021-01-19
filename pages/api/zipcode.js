00486fd6288434bb5f8d1cc2d321971b3cfe1641
cti1650 <cti1650@gmail.com>
2021-01-20 07:16:14 +0900

    updated zipcode

--- pages/api/zipcode.js
+++ pages/api/zipcode.js
@@ -1,7 +1,11 @@
 // /src/app/pages/api/user.ts
 export default async(req, res) => {
-    // 外部APIを呼び出し
-    const response = await fetch("http://zipcloud.ibsnet.co.jp/api/search?zipcode=2500011");
-    const data = await response.json();
-    res.status(200).json(data);
+    if(req.query.zipcode){
+        // 外部APIを呼び出し
+        const response = await fetch("http://zipcloud.ibsnet.co.jp/api/search?zipcode="+req.query.zipcode);
+        const data = await response.json();
+        res.status(200).json(data);
+    }else{
+        res.status(200).json({});
+    }
 }

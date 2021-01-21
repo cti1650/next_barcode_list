75ad6bf05c59367e86e3aae62f8fa691ffdfb09c
cti1650 <cti1650@gmail.com>
2021-01-22 04:33:23 +0900

    update FetchPost

--- components/FetchPost/FetchPost.js
+++ components/FetchPost/FetchPost.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
  
-export default class CodeLoop extends Component {
+export default class FetchPost extends Component {
   constructor(props) {
     super(props);
     this.state = {

3763c94b0ac0100acea8c8aaa15728835082f62e
cti1650 <cti1650@gmail.com>
2021-01-22 07:09:11 +0900

    update FetchPost

--- components/FetchPost/FetchPost.js
+++ components/FetchPost/FetchPost.js
@@ -12,52 +12,21 @@ export default class FetchPost extends Component {
     };
   }
  
-  // ボタン押下時処理
-  handleClick(e) {
-    let url = this.state.url;
-    let data = this.state.data;
-    let new_items = this.state.items;
-    const obj = {};
-    const method = "GET";
-    const body = Object.keys(obj).map((key)=>key+"="+encodeURIComponent(obj[key])).join("&");
-    const headers2 = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-      'Authorization': 'Bearer xoxp-64683967046-64683967078-1675081906353-7c33e05dddc58d7a54404909dfe2381b'
-    };
-    const headers = {
-      'Authorization': 'Bearer xoxp-64683967046-64683967078-1675081906353-7c33e05dddc58d7a54404909dfe2381b'
-    };
-    fetch(url, {method, headers})
-      .then((res)=> res.json())
-      .then((json)=>{
-        console.log(json);
+  callApi() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+      .then((res) => {
         this.setState({
-          items: json
+          items: res.json()
         });
       })
       .catch(console.error);
-  }
-  getData() {
-    axios
-      .get(this.state.url,{
-        headers: {
-          Authorization: "Bearer xoxp-64683967046-64683967078-1675081906353-7c33e05dddc58d7a54404909dfe2381b"
-        }
-      })
-      .then(results => {
-        const data = results.data.content;        
-        this.setState({
-          items: data,
-        });
-      });
   }
  
   render() {
     const { items } = this.state;
     return (
       <div>
-        <button onClick={e => this.getData()}>
+        <button onClick={e => this.callApi()}>
           取得
         </button>
         <div>

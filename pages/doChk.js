451a354ee8df7a1ebe8df7500e1599dcc23c1f24
cti1650 <cti1650@gmail.com>
2021-01-22 04:33:53 +0900

    update doChk

--- pages/doChk.js
+++ pages/doChk.js
@@ -1,7 +1,7 @@
 // pages/post.js
 
 import CodeLoop from "../components/CodeLoop/CodeLoop";
-import CodeLoop from "../components/FetchPost/FetchPost";
+import FetchPost from "../components/FetchPost/FetchPost";
 
 const Page = () => {
   return (

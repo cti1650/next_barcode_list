0c76c6553fb8682e7fbf04e762dbf2a118756f2c
cti1650 <cti1650@gmail.com>
2021-01-22 18:55:31 +0900

    del testfile

--- pages/src/test.js
+++
@@ -1,6 +0,0 @@
-async function callApi() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments');
-    const users = await res.json();
-    console.log(users);
-  };
-callApi();
